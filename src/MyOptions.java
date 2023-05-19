ed33cc28d993d3869b0b9c991d5b3bdddba6e245
Surprisemm <nikitamanzhukov@gmail.com>
2023-05-19 12:02:45 +0200

    commit test 2

--- src/MyOptions.java
+++ src/MyOptions.java
@@ -6,7 +6,7 @@ public class MyOptions extends JPanel {
     public MyOptions() {
         super();
 
-        // тест коммита
+        // тест коммита 2
 
         add(new JCheckBox("Итоги"));
     }
