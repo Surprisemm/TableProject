d6676caf36b463d9c21fbf986e9f57a6ca6148a5
Surprisemm <nikitamanzhukov@gmail.com>
2023-05-24 12:18:37 +0200

    MyTable создаются все столбцы и строки

--- src/MyTable.java
+++ src/MyTable.java
@@ -7,28 +7,101 @@ import java.awt.*;
 public class MyTable extends JTable {
 
     public Object[][] data;
+    private int tableStartCol = 0;
+    private int tableStartRow = 0;
+    private int tableEndCol;
+    private int tableEndRow;
 
+    private int rowData;
+    private int colData;
+    private int roundingData;
+    private boolean isTopHeader;
+    private boolean isBottomFooter;
+    private boolean isLeftHeader;
+    private boolean isRightFooter;
+    private boolean isRoundingCheck;
+    private String rightFooterData;
+    private String bottomFooterData;
+    private MyTableModel tableModel;
 
     public MyTable(int rowData, int colData, int roundingData, boolean isTopHeader, boolean isLeftHeader,
                    boolean isRightFooter, boolean isBottomFooter, boolean isRoundingCheck,
                    String rightFooterData, String bottomFooterData) {
 
-        // Обработка заголовков и итогов
-        if(isTopHeader){rowData++;}
-        if(isBottomFooter){rowData++;}
-        if(isLeftHeader){colData++;}
-        if(isRightFooter){colData++;}
+        this.rowData = rowData;
+        this.colData = colData;
+        this.roundingData = roundingData;
+        this.isTopHeader = isTopHeader;
+        this.isLeftHeader = isLeftHeader;
+        this.isRightFooter = isRightFooter;
+        this.isBottomFooter = isBottomFooter;
+        this.isRoundingCheck = isRoundingCheck;
+        this.rightFooterData = rightFooterData;
+        this.bottomFooterData = bottomFooterData;
 
+        // Обработка заголовков и итогов
+        configureTable();
 
         // Создание таблицы с моделью данных
-        data = new Object[rowData][colData];
+        data = new Object[this.rowData][this.colData];
 
-        MyTableModel tableModel = new MyTableModel(data);
+        //Добавление своей модели
+        tableModel = new MyTableModel(data);
 
+        //Обновление текста в заголовках
+        updateHeaderText();
+
+        //Удаление стандартных заголовков
+        setTableHeader(null);
+
+        //Установка модели для таблицы
         setModel(tableModel);
         setFillsViewportHeight(true);
     }
 
+    private void configureTable() {
+        if (isTopHeader) {
+            rowData++;
+            tableStartRow = 1;
+        }
+        if (isBottomFooter) {
+            rowData++;
+            tableEndRow = rowData - 2;
+        }
+        if (isLeftHeader) {
+            colData++;
+            tableStartCol = 1;
+        }
+        if (isRightFooter) {
+            colData++;
+            tableEndCol = colData - 2;
+        }
+    }
 
+    private void updateHeaderText(){
+        if (isTopHeader){
+
+                int n = 1;
+                for (int i = tableStartCol; i < colData; i++) {
+                    data[0][i] = "Столбец " + n;
+                    n++;
+                }
+                //tableModel.setRowEditable(0,false);
+        }
+        if (isLeftHeader){
+            int n = 1;
+            for (int i = tableStartRow; i < rowData ; i++) {
+                data[i][0] = "Строка " + n;
+                n++;
+            }
+        }
+        if(isBottomFooter){
+            data[rowData - 1][0] = "";
+            //tableModel.setRowEditable(rowData - 1,false);
+        }
+        if(isRightFooter){
+            data[0][colData - 1] = "";
+        }
+    }
 
 }
